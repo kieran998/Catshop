2aec01d2794913ac3ba479692a15f514eddff342
hToe <kieranlee998@gmail.com>
2023-01-31 06:54:54 +0000

    Added first JUnit test class

---
+++ catalogue/BetterBasketTest.java
@@ -0,0 +1,19 @@
+package catalogue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BetterBasketTest {
+
+	@Test
+	void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDetails() {
+		fail("Not yet implemented");
+	}
+
+}
